20a775ab81b9eaa1cf42250e540fd2df315d4869
Joshgun <joshgunimamalievi@icloud.com>
2024-12-01 17:30:44 +0100

    modified the file made it more readable

---
+++ cpp/firstcpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std; //means that we can use names for objects and variables from the standard library.
+
+int main() {
+    cout << "Hello world\n";// for printing (cout"see-out" <<) the text
+    cout << "How are you?" << "\n";// you can write "\n" also like this to be more readable
+    cout << "New line" << endl; // this is also for new line
+
+    cout << 5+5*2;
+
+    //c++ is case sensitive cout and Cout has different meanings
+    return 0;// ends the function
+}
